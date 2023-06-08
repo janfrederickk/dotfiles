9cf8d5e02a2ac86870d15ad971eeac1340d093ee
jan <wacker.jan@t-online.de>
2023-06-08 08:49:49 +0200

    add desktop images

---
+++ dot_config/scripts/executable_setbg.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+feh --bg-scale ~/.config/WidePeepoHappyDesktop/images/XPPeepo_M4x_Day.png ~/.config/WidePeepoHappyDesktop/images/XPPeepo_M4x_Day.png ~/.config/WidePeepoHappyDesktop/images/XPPeepo_M4x_Day.png
